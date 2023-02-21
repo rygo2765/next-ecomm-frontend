ff458a17511c48019c37857f53dc9b13e1a8bf7e
Ryan Goh <ryangohce@gmail.com>
2023-02-21 11:04:38 +0800

    created submit button and implemented POST to database

--- src/routes/users/new/+page.svelte
+++ src/routes/users/new/+page.svelte
@@ -1,6 +1,40 @@
 <script>
   import {PUBLIC_BACKEND_BASE_URL} from '$env/static/public';
+  import Button from '../../../components/Button.svelte';
 
+	// let formErrors = {};
+  let isFormSubmitting = false;
+
+	async function createUser(evt){
+		evt.preventDefault();
+		
+		// if (evt.target['password'].value != evt.target['password-confirmation'].value) {
+		// 	formErrors['password'] = {message: 'Password confirmation does not match'};
+		// 	return;
+		// }
+    
+		isFormSubmitting = true;
+
+		const userData = {
+			name: evt.target['username'].value,
+			email: evt.target['email'].value,
+			password: evt.target['password'].value
+		}
+
+		const resp = await fetch(PUBLIC_BACKEND_BASE_URL + '/users', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(userData)
+		});
+
+		if (resp.status == 200){
+			console.log(resp.body)
+		}
+
+	}
 </script>
 
 <svelte:head>
@@ -10,16 +44,16 @@
 <h1 class="text-center text-xl mt-5">Sign Up</h1>
 
 <div class="flex justify-center items-center mt-8">
-  <form class="w-3/4 sm:w-1/3">
+  <form on:submit={createUser} class="w-3/4 sm:w-1/3">
 
     <!--Name input-->
     <div class="form-control w-full">
-      <label class="label" for="name">
+      <label class="label" for="username">
 				<span class="label-text">Name</span>
 			</label>
 			<input
 				type="text"
-				name="Name"
+				name="username"
 				placeholder="johndoe"
 				class="input input-bordered w-full"
 			/>
@@ -66,6 +100,10 @@
 				required
 			/>
 		</div>
-    
+
+    <!--Create Account Button-->
+		<div class="form-control w-full mt-4">
+			<Button standard="Create an Account" loading="Creating Account" clicked={isFormSubmitting} />
+		</div>
   </form>
 </div>
