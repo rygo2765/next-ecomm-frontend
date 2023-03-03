6235d100b2fb8c72076568c0b1653c519a5d627f
Ryan Goh <ryangohce@gmail.com>
2023-03-03 16:26:53 +0800

    implemented image output as cards on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 import Upload from "../components/Upload.svelte";
 import {isLoggedIn} from "../utils/auth.js";
+export let data
 </script>
 
 {#if $isLoggedIn}
@@ -8,4 +9,22 @@ import {isLoggedIn} from "../utils/auth.js";
     <Upload/>
 </div>
 {/if}
+
+<div class="flex flex-wrap basis-1/3 flex-none justify-center mt-10">
+    {#each data.images as image}
+    <div class="card card-compact w-96 bg-base-100 shadow-xl mx-2">
+        <figure><img src={image.url} alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">{image.title}</h2>
+            <p>{image.description}</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        </div>
+    {/each}
+</div>
+
+
+
 
